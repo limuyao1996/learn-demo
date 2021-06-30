00e65bb53095f297ab33c34a30bbe5a279563ae5
limuyao1996 <911407614@qq.com>
2021-06-30 17:00:31 +0800

    测试independent模式

--- packages/module-1/index.js
+++ packages/module-1/index.js
@@ -8,4 +8,8 @@ module.exports.sayHi = function () {
     console.log('Hi! module1')
 }
 
+module.exports.sayHello = function () {
+    console.log('Hello! module1')
+}
+
 module.exports.Vue = Vue;
--- packages/module-2/index.js
+++ packages/module-2/index.js
@@ -2,7 +2,10 @@ module.exports.module2 = function () {
     console.log('this is module2')
 }
 
-
 module.exports.sayHi = function () {
     console.log('Hi! module2')
+}
+
+module.exports.sayHello = function () {
+    console.log('Hello! module2')
 }
