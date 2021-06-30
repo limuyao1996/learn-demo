b626516aa464515a30e79865afad3664c733536a
limuyao1996 <911407614@qq.com>
2021-06-30 16:57:15 +0800

    测试俩模块修改

--- packages/module-1/index.js
+++ packages/module-1/index.js
@@ -4,4 +4,8 @@ module.exports.module1 = function () {
     console.log('this is module1')
 }
 
+module.exports.sayHi = function () {
+    console.log('Hi! module1')
+}
+
 module.exports.Vue = Vue;
--- packages/module-2/index.js
+++ packages/module-2/index.js
@@ -1,3 +1,8 @@
 module.exports.module2 = function () {
     console.log('this is module2')
 }
+
+
+module.exports.sayHi = function () {
+    console.log('Hi! module2')
+}
